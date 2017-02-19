e25a40a51f7099ced33126777146bb4cb7f4f734
Victor Wibisono <vwibisono@dius.com.au>
2017-02-19 16:45:08 +0800

    Added memoization.

--- knapsack/README.md
+++ knapsack/README.md
@@ -1,3 +1,7 @@
 # knapsack
 
 This program aims to solve the knapsack problem.
+
+### launch.json
+
+`launch.json` file is used for debugging using VSCode.
--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -1,13 +1,40 @@
 # -*- coding: utf-8 -*-
 """Finds the most valuable items to put into a knapsack."""
 
-FILENAME = "products.csv"
+import functools
+
+FILENAME = "test-data-2.csv"
 TOTE_LENGTH = 45
 TOTE_WIDTH = 30
 TOTE_HEIGHT = 35
 TOTE_VOLUME = TOTE_LENGTH * TOTE_WIDTH * TOTE_HEIGHT
 
 PRODUCTS = []
+
+class Memoized(object):
+    '''Decorator. Caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned
+    (not reevaluated).'''
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        try:
+            return self.cache[args]
+        except KeyError:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+        except TypeError:
+           # uncachable -- for instance, passing a list as an argument.
+           # Better to not cache than to blow up entirely.
+            return self.func(*args)
+    def __repr__(self):
+        '''Return the function's docstring.'''
+        return self.func.__doc__
+    def __get__(self, obj, objtype):
+        '''Support instance methods.'''
+        return functools.partial(self.__call__, obj)
 
 def check_dimension(vol):
     """Checks that the item fits in the tote."""
@@ -16,18 +43,7 @@ def check_dimension(vol):
     else:
         return True
 
-def pick(products, capacity):
-    """Decides which products to pick."""
-    sortedproducts = sorted(products, key=lambda product: product['volume'])
-    j = capacity
-    result = []
-    for i in xrange(len(sortedproducts), 0, -1):
-        if bestvalue(i, j) != bestvalue(i - 1, j):
-            result.append(products[i - 1])
-            j -= products[i - 1]['volume']
-    result.reverse()
-    return result
-
+@Memoized
 def bestvalue(index, maxvol):
     """Returns the total price of the most valuable subsequence of the first
     i elements in products whose volume sum to no more than tote volume."""
@@ -40,6 +56,17 @@ def bestvalue(index, maxvol):
     else:
         return max(bestvalue(index - 1, maxvol - itemvolume) + value, # case that this item is taken
                    bestvalue(index - 1, maxvol)) # case that this item is not taken
+
+def pick(products, capacity):
+    """Decides which products to pick."""
+    j = capacity
+    result = []
+    for i in xrange(len(products), 0, -1):
+        if bestvalue(i, j) != bestvalue(i - 1, j):
+            result.append(products[i - 1])
+            j -= products[i - 1]['volume']
+    result.reverse()
+    return result
 
 with open(FILENAME, "rb") as f:
     for line in f:
